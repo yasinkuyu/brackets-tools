07a3d8d2bdd8c843afd846bac781b56c09f2aff8
Denisov21 <student@alice.it>
2014-06-30 17:30:12 +0200

    Create strings.js
    Add Italian language!

---
+++ nls/it/strings.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012 Yasin Kuyu - twitter.com/yasinkuyu. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ */
+
+// English - root strings
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    "MENU_LABEL"                       : "Strumenti",
+    "MENU_NEW_DOC"                     : "Nuovo Html5...",
+    "VIEW_HIGHLIGHT"                   : "Evidenzia Parole Selezionate",
+    "STRING_REMOVE_EMPTY_LINES"        : "Rimuovi righe vuote",
+    "STRING_REMOVE_BREAK_LINES"        : "Rimuovere linee di interruzione",
+    "STRING_REMOVE_LEADING_NEW_LINES"  : "Rimuovi le prime nuove linee",    
+    "STRING_REMOVE_LINE_NUMBERS"       : "Rimuovi numeri di riga",
+    "STRING_REMOVE_DUPLICATE_LINES"    : "Rimuovi righe duplicate",
+    "STRING_UPPERCASE"                 : "Maiuscolo",
+    "STRING_LOWERCASE"                 : "Minuscolo",
+    "STRING_TITLECASE"                 : "Per Titolo",
+    "STRING_HTML_ENCODE"               : "Codifica HTML",
+    "STRING_HTML_DECODE"               : "Decodifica HTML", 
+    "STRING_ENCODE_URI"                : "Codifica URL", 
+    "STRING_DECODE_URI"                : "Decodifica URL"
+});
