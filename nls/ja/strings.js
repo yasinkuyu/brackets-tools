1af593152cda23fb09aa95b1a364e1c87388b77a
Norihito Nishioka <abigail4649@gmail.com>
2015-07-03 20:45:18 +0900

    Japanese Translation

---
+++ nls/ja/strings.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2012 Yasin Kuyu - twitter.com/yasinkuyu. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ */
+
+// Japanese Translation @nishioka
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    "MENU_LABEL"                       : "ツール",
+    "MENU_NEW_DOC"                     : "HTML5ファイルを新規作成",
+    "VIEW_HIGHLIGHT"                   : "カーソルの単語をハイライト",
+    "STRING_REMOVE_EMPTY_LINES"        : "空行を削除",
+    "STRING_REMOVE_BREAK_LINES"        : "改行を取り除く",
+    "STRING_REMOVE_LINE_NUMBERS"       : "行番号を削除",
+    "STRING_REMOVE_DUPLICATE_LINES"    : "重複行を削除",
+    "STRING_UPPERCASE"                 : "大文字に変換",
+    "STRING_LOWERCASE"                 : "小文字に変換",
+    "STRING_TITLECASE"                 : "先頭を大文字に変換",
+    "STRING_HTML_ENCODE"               : "HTMLエンコード",
+    "STRING_HTML_DECODE"               : "HTMLデコード",
+    "STRING_ENCODE_URI"                : "URLエンコード",
+    "STRING_DECODE_URI"                : "URLデコード",
+    "STRING_HTML_TAG_STRIP"            : "HTMLタグの除去",
+    "STRING_TRIM"                      : "左右のスペースを削除",
+    "STRING_LTRIM"                     : "左側のスペースを削除",
+    "STRING_RTRIM"                     : "右側のスペースを削除",
+    "STRING_FxCHAR_REMOVE"             : "先頭のx字を削除",
+    "STRING_LxCHAR_REMOVE"             : "末尾のx字を削除",
+    "STRING_SEO_URL"                   : "ハイフン区切りに変換",
+    "STRING_WORD_TO_ARRAY"             : "単語を配列に変換",
+    "STRING_WORD_TO_LIST"              : "単語をリスト要素に変換",
+    "STRING_LINE_TO_LIST"              : "行をリスト要素に変換",
+    "INPUT_TITLE"                      : "文字を削除",
+    "INPUT_TEXT"                       : "削除する文字数を入力します。",
+    "SETTINGS"                         : "設定"
+});
